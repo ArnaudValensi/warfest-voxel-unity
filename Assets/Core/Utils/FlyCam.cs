31324b35348254538bb24570dcd548bd3bfb3386
Arnaud Valensi <arnaud.valensi@gmail.com>
2017-08-21 20:57:03 +0200

    prepare the first scene

---
+++ Assets/Core/Utils/FlyCam.cs
@@ -0,0 +1,90 @@
+﻿using UnityEngine;
+
+public class FlyCam : MonoBehaviour {
+	Vector3 movementVector;
+
+	public enum RotationAxes {
+		MouseXAndY = 0,
+		MouseX = 1,
+		MouseY = 2
+
+	}
+
+	public int speed = 7;
+	public int verticalSpeed = 7;
+
+	public RotationAxes axes = RotationAxes.MouseXAndY;
+	public float sensitivityX = 5F;
+	public float sensitivityY = 5F;
+	public float minimumY = -60F;
+	public float maximumY = 60F;
+
+	static float rotationY = 0F;
+
+	void Start() {
+		SetCursorLock(true);
+	}
+
+	void Update() {
+		KeyInput();
+
+		if (Cursor.lockState == CursorLockMode.Locked) {
+			MouseAxesInput();
+		}
+	}
+
+	void KeyInput() {
+		movementVector = Vector3.zero;
+
+		if (Input.GetKey(KeyCode.W))
+			movementVector += Camera.main.transform.forward * speed;
+
+		if (Input.GetKey(KeyCode.S))
+			movementVector -= Camera.main.transform.forward * speed;
+
+		if (Input.GetKey(KeyCode.A))
+			movementVector -= Camera.main.transform.right * speed;
+
+		if (Input.GetKey(KeyCode.D))
+			movementVector += Camera.main.transform.right * speed;
+
+		if (Input.GetKey(KeyCode.Space))
+			movementVector += Camera.main.transform.up * verticalSpeed;
+
+		if (Input.GetKey(KeyCode.LeftShift))
+			movementVector -= Camera.main.transform.up * verticalSpeed;
+
+		if (Input.GetKeyDown(KeyCode.Escape)) {
+			SetCursorLock(false);
+		}
+
+		if (Input.GetMouseButtonDown(0)) {
+			SetCursorLock(true);
+		}
+
+		Camera.main.transform.position = Camera.main.transform.position + movementVector * Time.deltaTime;
+	}
+
+	void MouseAxesInput() {
+		if (axes == RotationAxes.MouseXAndY) {
+			float rotationX = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * sensitivityX;
+
+			rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+			rotationY = Mathf.Clamp(rotationY, minimumY, maximumY);
+
+			transform.localEulerAngles = new Vector3(-rotationY, rotationX, 0);
+		} else if (axes == RotationAxes.MouseX) {
+			transform.Rotate(0, Input.GetAxis("Mouse X") * sensitivityX, 0);
+		} else {
+			rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+			rotationY = Mathf.Clamp(rotationY, minimumY, maximumY);
+
+			transform.localEulerAngles = new Vector3(-rotationY, transform.localEulerAngles.y, 0);
+		}
+	}
+
+	void SetCursorLock(bool locked) {
+		Cursor.lockState = (locked) ? CursorLockMode.Locked : CursorLockMode.None;
+		Cursor.visible = CursorLockMode.Locked != Cursor.lockState;
+	}
+}
