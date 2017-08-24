34884e3a4545df983298ac2fa84f8d544b72c791
Arnaud Valensi <arnaud.valensi@gmail.com>
2017-08-24 14:31:14 +0200

    add some skyboxes

---
+++ Assets/Core/Skyboxes/Scripts/Skybox/SkyboxChanger.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class SkyboxChanger : MonoBehaviour
+{
+    public Material[] Skyboxes;
+    private Dropdown _dropdown;
+
+    public void Awake()
+    {
+        _dropdown = GetComponent<Dropdown>();
+        //var options = Skyboxes.Select(skybox => skybox.name).ToList();
+        //_dropdown.AddOptions(options);
+    }
+
+    public void ChangeSkybox()
+    {
+        RenderSettings.skybox = Skyboxes[_dropdown.value];
+        RenderSettings.skybox.SetFloat("_Rotation", 0);
+    }
+}
