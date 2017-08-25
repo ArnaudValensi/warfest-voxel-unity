fbe5e67c6f989d85afd92cfc827256532df250dc
Arnaud Valensi <arnaud.valensi@gmail.com>
2017-08-25 23:37:33 +0200

    add platform highlight

---
+++ Assets/Core/Scripts/WorldObjects/PlayerDetect.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+namespace Warfest {
+	public class PlayerDetect : MonoBehaviour {
+
+		[SerializeField]
+		Material onPlayerMaterial = null;
+
+		MeshRenderer meshRenderer;
+		Material originalMaterial;
+
+		void Start() {
+			meshRenderer = transform.parent.GetComponent<MeshRenderer>();
+		}
+
+		void OnTriggerEnter(Collider other) {
+			if (other.GetComponent<Player>() == null) {
+				return;
+			}
+
+			originalMaterial = meshRenderer.sharedMaterial;
+			meshRenderer.sharedMaterial = onPlayerMaterial;
+		}
+
+		void OnTriggerExit(Collider other) {
+			if (other.GetComponent<Player>() == null) {
+				return;
+			}
+
+			meshRenderer.sharedMaterial = originalMaterial;
+		}
+
+	}
+}
