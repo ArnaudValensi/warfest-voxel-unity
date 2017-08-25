1a8eea051357563f5610386b96e5d81b93552461
Arnaud Valensi <arnaud.valensi@gmail.com>
2017-08-25 03:20:51 +0200

    add some player controls

---
+++ Assets/Core/Scripts/Player/Player.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace Warfest {
+	public class Player : MonoBehaviour {
+		public float movementSpeed = 10;
+		public float turningSpeed = 60;
+
+		Animator animator;
+		int speedHash;
+
+		void Start() {
+			animator = GetComponent<Animator>();
+			speedHash = Animator.StringToHash("Speed");
+		}
+
+		void Update() {
+			Vector2 movement = new Vector2(
+				Input.GetAxis("Horizontal"),
+				Input.GetAxis("Vertical")
+			);
+			float horizontal = movement.x * turningSpeed * Time.deltaTime;
+			transform.Rotate(0, horizontal, 0);
+
+			float vertical = movement.y * movementSpeed * Time.deltaTime;
+			transform.Translate(0, 0, vertical);
+
+			animator.SetFloat(speedHash, movement.y);
+		}
+	}
+}
