3657279950fcace99e9ce5dd84068987c6dd5669
Arnaud Valensi <arnaud.valensi@gmail.com>
2017-08-25 17:25:26 +0200

    improve third person camera

--- Assets/Core/Scripts/Camera/ThirdPersonCamera.cs
+++ Assets/Core/Scripts/Camera/ThirdPersonCamera.cs
@@ -3,80 +3,89 @@
 namespace Warfest {
 	public class ThirdPersonCamera : MonoBehaviour {
 
-		[SerializeField]
-		Transform followTransform = null;
-
-//		[SerializeField]
-//		Vector3 offset = new Vector3(0f, 1.5f, 0f);
-
-//		[SerializeField]
-//		float rotateSpeed = 1f;
-
-		[SerializeField]
-		float distanceUp = 2f;
-
+		//		[SerializeField]
+		//		float distanceUp = 2f;
+		
 		[SerializeField]
 		float distanceAway = 3f;
 
-		Vector3 lookDir;
-		Vector3 targetPosition;
-
-		// Smoothing and damping
-		Vector3 velocityCamSmooth = Vector3.zero;
 		[SerializeField]
-		float camSmoothDampTime = 0.1f;
+		Transform followTransform = null;
 
+		[Range(0f, 10f)]
+		[SerializeField]
+		float m_TurnSpeed = 1.5f;
+		// How fast the rig will rotate from user input.
+
+		[SerializeField]
+		float followSpeed = 1f;
+
+		[SerializeField]
+		float m_TiltMax = 75f;
+		// The maximum value of the x axis rotation of the pivot.
+
+		[SerializeField]
+		float m_TiltMin = 45f;
+		// The minimum value of the x axis rotation of the pivot.
+
+		float m_LookAngle;
+		// The rig's y axis rotation.
+		float m_TiltAngle;
+		// The pivot's x axis rotation.
+		const float k_LookDistance = 100f;
+		// How far in front of the pivot the character's look target is.
+		Vector3 m_PivotEulers;
+		Quaternion m_PivotTargetRot;
+		Quaternion m_TransformTargetRot;
+		Transform m_Pivot;
+		Transform mainCamera;
+
+		void Awake() {
+			m_Pivot = transform.Find("Pivot");
+			mainCamera = m_Pivot.Find("Main Camera");
+
+			m_PivotEulers = m_Pivot.rotation.eulerAngles;
+			m_PivotTargetRot = m_Pivot.transform.localRotation;
+			m_TransformTargetRot = transform.localRotation;
+
+			Debug.Log(distanceAway);
+
+			mainCamera.localPosition = new Vector3(mainCamera.position.x, mainCamera.position.y, -distanceAway);
+		}
+
+		void Update() {
+			HandleRotationMovement();
+		}
 
 		void LateUpdate() {
-//			Vector2 mouseInput = new Vector2(
-//				Input.GetAxis("Mouse X"),
-//				Input.GetAxis("Mouse Y")
-//			);
-//
-//			Debug.Log(mouseInput);
-
-			// Allow to target the head of the character instead of the feet
-//			Vector3 characterOffset = followTransform.position + offset;
-			Vector3 followPos = followTransform.position;
-
-			// Calculate direction from camera to player, kill Y, and normalize to give a valid direction
-			// with unit magnitude.
-			lookDir = followPos - transform.position;
-			lookDir.y = 0;
-			lookDir.Normalize();
-
-
-			// Setting the target position to be the correct offset from the target.
-			targetPosition = followPos + Vector3.up * distanceUp - lookDir * distanceAway;
-
-			Debug.DrawRay(transform.position, lookDir, Color.green);
-			Debug.DrawLine(followTransform.position, targetPosition, Color.magenta);
-
-			CompensateForWalls(followPos, ref targetPosition);
-			SmoothPostion(transform.position, targetPosition);
-
-			// Make sure the camera is looking the right way.
-			transform.LookAt(followTransform);
+			// Follow target.
+			transform.position = followTransform.position;
 		}
 
-		void SmoothPostion(Vector3 fromPos, Vector3 toPos) {
-			// Making a smooth transition between the camera current position and the position
-			// we want to be at.
-			transform.position = Vector3.SmoothDamp(fromPos, toPos, ref velocityCamSmooth, camSmoothDampTime);
-
-			// This line is an alternative way to smooth
-			//transform.position = Vector3.Lerp(transform.position, targetPosition, Time.deltaTime * smooth);
-		}
-
-		void CompensateForWalls(Vector3 fromObject, ref Vector3 toTarget) {
-			Debug.DrawLine(fromObject, toTarget, Color.cyan);
-
-			// Compensate for walls between camera
-			RaycastHit wallHit;
-			if (Physics.Linecast(fromObject, toTarget, out wallHit)) {
-				Debug.DrawRay(wallHit.point, Vector3.left, Color.red);
-				toTarget = new Vector3(wallHit.point.x, toTarget.y, wallHit.point.z);
+		void HandleRotationMovement() {
+			if (Time.timeScale < float.Epsilon) {
+				return;
 			}
+
+			float x = Input.GetAxis("Mouse X");
+			float y = Input.GetAxis("Mouse Y");
+
+			// Adjust the look angle by an amount proportional to the turn speed and horizontal input.
+			m_LookAngle += x * m_TurnSpeed;
+
+			// Rotate the rig (the root object) around Y axis only:
+			m_TransformTargetRot = Quaternion.Euler(0f, m_LookAngle, 0f);
+
+			// on platforms with a mouse, we adjust the current angle based on Y mouse input and turn speed
+			m_TiltAngle -= y * m_TurnSpeed;
+			// and make sure the new value is within the tilt range
+			m_TiltAngle = Mathf.Clamp(m_TiltAngle, -m_TiltMin, m_TiltMax);
+
+			// Tilt input around X is applied to the pivot (the child of this object)
+			m_PivotTargetRot = Quaternion.Euler(m_TiltAngle, m_PivotEulers.y, m_PivotEulers.z);
+
+			m_Pivot.localRotation = m_PivotTargetRot;
+			transform.localRotation = m_TransformTargetRot;
 		}
 
 	}
