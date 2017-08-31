ae93e53efbdb7aa11a44ba9f2b0f6a64600be27b
Arnaud Valensi <arnaud.valensi@gmail.com>
2017-08-31 16:00:44 +0200

    make preview model in ui rotating

---
+++ Assets/Core/Components/Inventory/Scripts/AutoRotate.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Warfest {
+	public class AutoRotate : MonoBehaviour {
+
+		[SerializeField]
+		float speed = 1f;
+
+		void Update() {
+			transform.RotateAroundY(speed * Time.deltaTime);
+		}
+
+	}
+}
