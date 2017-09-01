fe9b79f77f6c60be10165351d8dbfd9d048d270b
Arnaud Valensi <arnaud.valensi@gmail.com>
2017-09-01 16:33:49 +0200

    move RLE in SaveVoxelTerrain

--- Assets/Core/Scripts/Voxel/VoxelTerrain/SaveVoxelTerrain.cs
+++ Assets/Core/Scripts/Voxel/VoxelTerrain/SaveVoxelTerrain.cs
@@ -65,19 +65,44 @@ namespace Warfest {
 				writer.Write(posY);
 				writer.Write(posZ);
 
-				RunLengthEncoding.RLEEncode(chunk.voxels, writer, sizeX, sizeY, sizeZ);
+				RLEEncode(chunk.voxels, writer, sizeX, sizeY, sizeZ);
 			}
 		}
 
-		byte[] SerializeColor32(Color32 color) {
-			byte[] colorArray = new byte[4];
+		// Run Length Encoding
+		static void RLEEncode(Voxel[,,] voxels, BinaryWriter writer, int sizeX, int sizeY, int sizeZ) {
+			int lastColor = 0;
+			int currentColor;
+			short consecutiveTypes = 0;
+			byte lastType = byte.MaxValue;
+			byte currentType;
 
-			colorArray[0] = color.r;
-			colorArray[1] = color.g;
-			colorArray[2] = color.b;
-			colorArray[3] = color.a;
+			for (int z = 0; z < sizeZ; z++) {
+				for (int y = 0; y < sizeY; y++) {
+					for (int x = 0; x < sizeX; x++) {
+						Voxel voxel = voxels[x, y, z];
 
-			return colorArray;
+						currentColor = Color32ToInt(voxel.color);
+						currentType = (byte)voxel.type;
+						if (currentColor == lastColor && currentType == lastType) {
+							consecutiveTypes++;
+						} else {
+							if (consecutiveTypes > 0) {
+								writer.Write(consecutiveTypes);
+								writer.Write(lastType);
+								writer.Write(lastColor);
+							}
+							lastColor = currentColor;
+							lastType = currentType;
+							consecutiveTypes = 1;
+						}
+					}
+				}
+			}
+
+			writer.Write(consecutiveTypes);
+			writer.Write(lastType);
+			writer.Write(lastColor);
 		}
 
 		//----------------------------------------------------------------------------
@@ -111,7 +136,7 @@ namespace Warfest {
 				int posZ = reader.ReadInt32();
 
 
-				VoxelData[,,] voxelsData = RunLengthEncoding.RLEDecode(reader, sizeX, sizeY, sizeZ);
+				VoxelData[,,] voxelsData = RLEDecode(reader, sizeX, sizeY, sizeZ);
 
 				for (int z = 0; z < sizeZ; z++) {
 					for (int y = 0; y < sizeY; y++) {
@@ -132,12 +157,64 @@ namespace Warfest {
 			}
 		}
 
-		Color32 DeserializeColor32(byte[] colorArray) {
+		// Run Length Encoding Decoding
+		static VoxelData[,,] RLEDecode(BinaryReader binaryReader, int sizeX, int sizeY, int sizeZ) {
+			var voxelsData = new SaveVoxelTerrain.VoxelData[sizeX, sizeY, sizeZ];
+			int x = 0;
+			int y = 0;
+			int z = 0;
+			byte type;
+			int intColor;
+			Color32 color;
+			short count;
+			int length = sizeX * sizeY * sizeZ;
+
+			for (int i = 0; i < length;) {
+				count = binaryReader.ReadInt16();
+				type = binaryReader.ReadByte();
+				intColor = binaryReader.ReadInt32();
+				color = IntToColor32(intColor);
+
+				for (int j = 0; j < count; j++) {
+					voxelsData[x, y, z] = new SaveVoxelTerrain.VoxelData {
+						type = (Voxel.Type)type,
+						color = color
+					};
+
+					x = (x + 1) % sizeX;
+
+					if (x == 0) {
+						y = (y + 1) % sizeY;
+
+						if (y == 0) {
+							z++;
+						}
+					}
+
+					i++;
+				}
+			}
+
+			return voxelsData;
+		}
+
+		static int Color32ToInt(Color32 color) {
+			int intColor;
+
+			intColor = color.r << 24;
+			intColor += color.g << 16;
+			intColor += color.b << 8;
+			intColor += color.a;
+
+			return intColor;
+		}
+
+		static Color32 IntToColor32(int intColor) {
 			return new Color32(
-				colorArray[0],
-				colorArray[1],
-				colorArray[2],
-				colorArray[3]
+				(byte)((intColor >> 24) & 0xFF),
+				(byte)((intColor >> 16) & 0xFF),
+				(byte)((intColor >> 8) & 0xFF),
+				(byte)(intColor & 0xFF)
 			);
 		}
 
