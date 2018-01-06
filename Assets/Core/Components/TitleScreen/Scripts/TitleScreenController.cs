8b2758aa4076510fce6d7b771c6edab110ff044a
Arnaud Valensi <arnaud.valensi@gmail.com>
2018-01-06 13:43:54 +0100

    add transition between title and map selection screen

---
+++ Assets/Core/Components/TitleScreen/Scripts/TitleScreenController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class TitleScreenController : MonoBehaviour {
+
+	[SerializeField] GameObject titleCanvas;
+	[SerializeField] GameObject mapSelectionCanvas;
+
+	public void GoToMapSelection() {
+		titleCanvas.SetActive(false);
+		mapSelectionCanvas.SetActive(true);
+	}
+
+	public void GoToTitle() {
+		mapSelectionCanvas.SetActive(false);
+		titleCanvas.SetActive(true);
+	}
+
+}
