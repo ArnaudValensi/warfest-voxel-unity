4770991f1864210c5b2ca57136d2b0ecce4200c3
Arnaud Valensi <arnaud.valensi@gmail.com>
2018-01-06 15:20:38 +0100

    improve new world creation screen

--- Assets/Core/Components/TitleScreen/Scripts/TitleScreenController.cs
+++ Assets/Core/Components/TitleScreen/Scripts/TitleScreenController.cs
@@ -4,15 +4,22 @@ public class TitleScreenController : MonoBehaviour {
 
 	[SerializeField] GameObject titleCanvas;
 	[SerializeField] GameObject mapSelectionCanvas;
+	[SerializeField] GameObject newWorldCanvas;
 
 	public void GoToMapSelection() {
 		titleCanvas.SetActive(false);
+		newWorldCanvas.SetActive(false);
 		mapSelectionCanvas.SetActive(true);
 	}
 
 	public void GoToTitle() {
 		mapSelectionCanvas.SetActive(false);
 		titleCanvas.SetActive(true);
+	}
+
+	public void GoToNewWorldCreation() {
+		mapSelectionCanvas.SetActive(false);
+		newWorldCanvas.SetActive(true);
 	}
 
 }
