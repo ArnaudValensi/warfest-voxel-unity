0d55dea5398d14930b5090ac3613295fe0a6cc7d
Arnaud Valensi <arnaud.valensi@gmail.com>
2018-01-06 15:02:03 +0100

    improve text input

---
+++ Assets/Core/Components/TitleSelectMap/Scripts/InputField.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class InputField : MonoBehaviour {
+
+	[SerializeField] GameObject unselectedLine;
+	[SerializeField] GameObject selectedLine;
+
+	public void OnSelect() {
+		unselectedLine.SetActive(false);
+		selectedLine.SetActive(true);
+	}
+
+	public void OnDeselect() {
+		selectedLine.SetActive(false);
+		unselectedLine.SetActive(true);
+	}
+
+}
