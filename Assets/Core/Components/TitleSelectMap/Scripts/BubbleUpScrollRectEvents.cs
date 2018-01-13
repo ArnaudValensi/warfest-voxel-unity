0672bf26575b3faa9f40cc18680fa821083c83ff
Arnaud Valensi <arnaud.valensi@gmail.com>
2018-01-13 16:01:10 +0100

    add scroll to map selection

---
+++ Assets/Core/Components/TitleSelectMap/Scripts/BubbleUpScrollRectEvents.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class BubbleUpScrollRectEvents: MonoBehaviour, IBeginDragHandler,  IDragHandler, IEndDragHandler, IScrollHandler {
+	ScrollRect mainScroll;
+
+	void Start() {
+		mainScroll = transform.GetComponentInParent<ScrollRect>();
+	}
+
+	public void OnBeginDrag(PointerEventData eventData) {
+		mainScroll.OnBeginDrag(eventData);
+	}
+
+
+	public void OnDrag(PointerEventData eventData) {
+		mainScroll.OnDrag(eventData);
+	}
+
+	public void OnEndDrag(PointerEventData eventData) {
+		mainScroll.OnEndDrag(eventData);
+	}
+
+
+	public void OnScroll(PointerEventData data) {
+		mainScroll.OnScroll(data);
+	}
+}
